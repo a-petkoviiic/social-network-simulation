355057b3d968463ba394d4f0d76fa7ae6f791ce8
Andjela Petkovic <andjelapetkovic1401@gmail.com>
2026-06-26 01:18:45 +0200

    Function for adding new following connection

--- social_graph.py
+++ social_graph.py
@@ -20,6 +20,9 @@ class SocialGraph(object):
 
         self.bio_words = dict()
 
+        self.following_history = dict() # kad on nekog zaprati dodaje se ovde kao kljuc
+        self.followers_history = dict() # kad neko njega zaprati dodaje se ovde kao kljuc
+
 
     # UCITAVANJE PODATAKA IZ FAJLOVA
 
@@ -120,7 +123,7 @@ class SocialGraph(object):
                 total_diff += abs(old_ranks[user_id] - new_ranks[user_id])
 
             if total_diff < epsilon:
-                print(f"\n{rounds} rundi")
+                #print(f"\n{rounds} rundi")
                 self.page_ranks = new_ranks
                 self.top_influencers = self.top_heap(self.users) # atribut klase dobija vrednost 5 najuticajnijih nakon izracunavanja page ranka
                 return
@@ -184,33 +187,126 @@ class SocialGraph(object):
 
         return self.top_heap(founded_matches, n)
 
+    # ------------------------------------------------------------------------------------------
+
+
+    # NOVA PRACENJA
+
+    def add_connection(self, user1_id, user2_id):
+        if user1_id not in self.users or user2_id not in self.users:
+            print("Jedan ili oba korisnika ne postoje.\n")
+            return False
+
+        if user2_id in self.following.get(user1_id, set()):
+            print("Veza vec postoji.\n")
+            return False
+
+        # azuriram following i followers
+        if user1_id not in self.following:
+            self.following[user1_id] = set()
+        self.following[user1_id].add(user2_id)
+        if user2_id not in self.followers:
+            self.followers[user2_id] = set()
+        self.followers[user2_id].add(user1_id)
+
+        # korisnik2 je zapracen od korisnika1 - ide u followers_history[user2_id]
+        if user2_id not in self.followers_history:
+            self.followers_history[user2_id] = []
+        self.followers_history[user2_id].append(user1_id)
+
+        # korisnik1 je zapratio korisnika2 - ide u following_history[user1_id]
+        if user1_id not in self.following_history:
+            self.following_history[user1_id] = []
+        self.following_history[user1_id].append(user2_id)
+
+        self.calculate_page_rank()
+        return True
+
+    def user_following_history(self, user_id):
+        return self.following_history[user_id]
+
 
 if __name__ == "__main__":
-    import time
-
     graph = SocialGraph()
     graph.load_all()
 
-    start = time.time()
     graph.calculate_page_rank()
-    end = time.time()
 
-    print(f"Vreme racunanja PageRank-a: {end - start:.4f} sekundi")
+    # print(f"Vreme racunanja PageRank-a: {end - start:.4f} sekundi")
 
-    print("\nTop 5 najuticajnijih korisnika: ")
-    for rank, user_id in graph.top_influencers:
-        username = graph.users[user_id].username
-        print(f"\t- {username}: {rank:.6f}")
+    while True:
+        print("\nMeni:")
+        print("1) Pretraga korisnika\n"
+              "2) Prikaz top 5 najuticajnijih korisnika\n"
+              "3) Dodavanje nove veze praćenja\n"
+              "4) Prikaz istorije praćenja\n"
+              "x za izlaz iz programa")
 
-    username = input("\nUnsetite username za search: ")
-    bio = input("Unsetite bio za search: ")
+        option = input("\nUnesite broj ispred željene opcije: ")
 
-    search = graph.search_users(username, bio, 5)
+        if option not in ["1", "2", "3", "4", "x"]:
+            print("Neispravan unos, pokusajte ponovo.\n")
 
-    if search:
-        print("Pronadjeni: ")
-        for rank, user_id in graph.search_users(username, bio, 5):
-            username = graph.users[user_id].username
-            print(f"\t- {username}: {rank:.6f}")
-    else:
-        print("Nema pronadjenih")
+        elif option == "1":
+            username = input("\nUsername: ")
+            bio = input("Bio: ")
+            while True:
+                top_num = input("Top num: ")
+                if not top_num.isdigit():
+                    print("Neispravan unos, pokusajte ponovo.\n")
+                    continue
+                break
+            search = graph.search_users(username, bio, int(top_num))
+
+            if search:
+                print("Pronadjeni: ")
+                for rank, user_id in search:
+                    username = graph.users[user_id].username
+                    print(f"\t- {username}: {rank:.6f}")
+            else:
+                print("Nema pronadjenih.")
+
+        elif option == "2":
+            print("\nTop 5 najuticajnijih korisnika: ")
+            for rank, user_id in graph.top_influencers:
+                username = graph.users[user_id].username
+                print(f"\t- {username}: {rank:.6f}")
+
+        elif option == "3":
+            while True:
+                new_connection = input("\nUnesite novu vezu u formatu follower_id->following_id: ")
+                if '->' not in new_connection:
+                    print("Neispravan unos, pokusajte ponovo")
+                    continue
+                break
+
+            users_id = new_connection.strip().split('->')
+            if len(users_id) != 2:
+                print("Neispravan unos, pokusajte ponovo")
+                continue
+
+            success = graph.add_connection(users_id[0], users_id[1])
+
+            if success:
+                print(f"{graph.users[users_id[0]].username} je zapratio {graph.users[users_id[1]].username}!")
+            else:
+                # vec ispisana poruka zasto u funkciji
+                continue
+
+        elif option == "4":
+            user_id = input("\nUnesite user_id korisnika ciju istoriju zelite da pregledate: ")
+            if user_id not in graph.users:
+                print("Trazeni korisnik ne postoji.")
+                continue
+            if user_id not in graph.following_history:
+                print("U ovoj sesiji ovaj korisnik nije zapratio nikoga.")
+                continue
+
+            following_history = graph.following_history[user_id]
+            print(f"Hronoloski prikazano koga je zapraćivao korisnik {graph.users[user_id].username}: ")
+            for user_id in following_history:
+                print(f"\t- {graph.users[user_id].username}")
+
+        elif option == "x":
+            print("\nIzlazak iz programa...")
+            break
